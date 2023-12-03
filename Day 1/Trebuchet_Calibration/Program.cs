6df2e44bbf646afec0d82d74b801ad5893685887
krandor <krandor.delmarniol@gmail.com>
2023-12-02 23:10:57 -0500

    add Day 1 and 2

---
+++ Day 1/Trebuchet_Calibration/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AdventOfCode2023;
+
+using System.IO.Abstractions;
+
+internal class Program
+{
+    private static IFileSystem FileSystem;
+
+    public static async Task Main(string[] args)
+    {
+        FileSystem = new FileSystem();
+        var lines = await FileSystem.File.ReadAllLinesAsync("input.txt");
+
+        var inputParser = new InputParser();
+
+        var total = inputParser.Parse(lines);
+
+        Console.WriteLine($"Total: {total}");
+    }
+}
